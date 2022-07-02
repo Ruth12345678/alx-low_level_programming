1c8bc0bcec8a052c469211233874d893159b1872
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-01 17:49:31 -0700

    Maybe

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
 #include<string.h>
+
 /*
  * main-prints out the specified string
  *
  * return-returns zero
  */
+
 int main(void)
 {
 	char stra[250];
 
 	strcpy(stra, "\"Programming is like building a multilingual puzzle");
 	puts(stra);
+	
 	return (0);
 }
