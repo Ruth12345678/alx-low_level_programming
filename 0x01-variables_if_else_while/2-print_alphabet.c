1d2c11589208f378db416a19cd9856928c77ba04
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-01 21:36:06 -0700

    Alph

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return - returns zero
+ */
+
+int main(void)
+{
+	char alph;
+	for(alph = 'a'; alph <= 'z'; alph++)
+		putchar(alph);
+		putchar("\n");
+	return (0);
+}
