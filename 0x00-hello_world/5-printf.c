be96c20fc76b9c290d39749527856e83065ce3c0
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-01 17:58:41 -0700

    Betty style knowledge test

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,14 @@
-/* C pritnf function*/
 #include<stdio.h>
+
+/**
+ * main - returns a string
+ *
+ * Return - Returns zero
+ */
 
 int main(){
 	char str[] = "with proper grammar, but the outcome is a piece of art,\n";
+
 	printf("%s",str);
 	return(0);
 }
