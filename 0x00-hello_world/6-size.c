7d564f41bafdfe27feaf7061335df29c6d2d6b96
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-01 18:11:53 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,10 +4,11 @@
 /**
  * main - prints out a formatted string
  *
- * Returns: zero
+ * Return: returns zero
  */
 
-int main(void){
-	printf("Size of a char: %i byte(s)\nSize of an int: %i byte(s)\nSize of a long int: %i byte(s)\nSize of a long long int: %i byte(s)\nSize of a float: %i byte(s)\n",sizeof(char),sizeof(int),sizeof(long int),sizeof(long long int),sizeof(float));
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\nSize of an int: %i byte(s)\nSize of a long int: %i byte(s)\nSize of a long long int: %i byte(s)\nSize of a float: %i byte(s)\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
 	return (0);
 }	
