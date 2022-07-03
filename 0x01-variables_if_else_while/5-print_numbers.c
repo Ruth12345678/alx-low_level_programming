22bf26c754b15809374612402e975a3af89511b9
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-03 11:04:20 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * main - entry point
+ *
+ * Return: returns zero as a success value
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%i", a);
+	}
+	putchar('\n');
+	return (0);
+}
