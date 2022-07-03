8495172366a79730144c8d249bccb71d532915bd
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-03 14:48:12 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * main - entry point
+ *
+ * Return: exit point, zero is success
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		
+		if (a != 9)
+		{
+			putchar(',');
+		}
+		else
+		{
+		}
+	}
+	putchar('\n');
+}
