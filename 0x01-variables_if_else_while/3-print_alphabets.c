a06e331ae51cb869e9cc0803a82ca4c170e413ef
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-03 09:52:15 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 /**
  * main - entry point
  *
@@ -16,7 +17,7 @@ int main(void)
 	for (lets = 'A'; lets <= 'Z'; lets++)
 	{
 		putchar(lets);
-		putchar("\n");
 	}
+	putchar('\n');
 	return (0);
 }
