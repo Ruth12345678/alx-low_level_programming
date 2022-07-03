9343a90c6ec095de0d44eb6f95601ddb6dc79041
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-03 15:51:36 -0700

    100.1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,18 +9,25 @@
 
 int main(void)
 {
-	int a,b;
+	int a, b;
 
 	for (a = 0; a <= 8; a++)
 	{
 		for (b = 1; b <= 9; b++)
 		{
-			if(b > a)
+			if (b > a)
 			{
 				putchar(a + '0');
 				putchar(b + '0');
-				putchar(',');
-				putchar(' ');
+				if (a != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				else
+				{
+
+				}
 			}
 			else
 			{
