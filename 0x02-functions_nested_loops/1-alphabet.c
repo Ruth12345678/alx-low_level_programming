4886fa6d4d829f286fadce14f4e9189e5d9645a4
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-05 15:53:13 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: exit point
+ */
+
+void print_alphabet(void)
+{
+	char ab;                                                                                                                                                                                                                                 
+        for(ab = 'a'; ab <= 'z'; ab++)                                                                                                  
+        {                                                                                                                               
+                putchar(ab);                                                                                                            
+        }                                                                                                                               
+
+	putchar('\n');                                                                                                                  
+
+}  
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #include<stdio.h>
+void print_alphabet(void); 
 
 int printa(void)
 {
@@ -6,14 +7,4 @@ int printa(void)
 
 	printf("%s\n", a);
 	return (0);
-}
-
-void print_alphabet(void){
-	char ab;
-
-	for(ab = 'a'; ab <= 'z'; ab++)
-	{
-		putchar(ab);
-	}
-	putchar('\n');
 }
