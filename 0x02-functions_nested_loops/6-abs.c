276d6acbeaa38342fdab1a7d7280e0995d1b52d7
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-06 21:40:07 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes absoslute value
+ *
+ * @n: input character
+ * Return: zero
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (n * -1);
+}
