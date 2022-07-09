2cb135a7e57063f9a1c16997a7b85415198be879
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-08 22:20:43 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,19 +18,19 @@ void times_table(void)
 		{
 			if (b < 9)
 			{
-				if (a == 2 && b >= 5)
+				if (a == 2 && b >= 4)
 				{
 					printf("%i, ", a * b);
 				}
-				else if (a == 3 && b >= 4)
+				else if (a == 3 && b >= 3)
 				{
 					printf("%i, ", a * b);
 				}
-				else if (a == 4 && b >= 3)
+				else if (a == 4 && b >= 2)
 				{
 					printf("%i, ", a * b);
 				}
-				else if (a >= 5 && b >= 2)
+				else if (a >= 5 && b >= 1)
 				{
 					printf("%i, ", a * b);
 				}
