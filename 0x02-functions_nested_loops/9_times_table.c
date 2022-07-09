40ac3e1b1aa1802ab18fa6eb5fdf446d5be718c9
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-08 21:26:32 -0700

    9

---
+++ 0x02-functions_nested_loops/9_times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * times_table - times table with comma
+ *
+ * Return: zero
+ */
+
+void times_table(void)
+{
+	int a, b;
+
+	a = 0;
+	while (a < 10)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			printf("%i," a * b);
+		}
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar('\n');
+	}
+}
