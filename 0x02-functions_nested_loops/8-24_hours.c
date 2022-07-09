2ff383e04902509b892fc6003a3559c888854292
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-08 21:13:51 -0700

    add

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,12 +8,14 @@
 
 void jack_bauer(void)
 {
-	int a, b
+	int a, b;
 
 	a = 0;
 	while (a < 24)
+	{
 		b = 0;
 		while (b < 60)
+		{
 			_putchar((a / 10) + '0');
 			_putchar((a % 10) + '0');
 			_putchar(':');
@@ -21,5 +23,7 @@ void jack_bauer(void)
 			_putchar((b % 10) + '0');
 			_putchar('\n');
 			b++;
+		}
 		a++;
+	}
 }	
