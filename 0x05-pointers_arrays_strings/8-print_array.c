a9eb245d293aed9c90aa34a304647b8d715645c2
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-12 18:53:01 -0700

    s

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * print_array - print certan array integers
+ *
+ * @a: int type array pointer
+ *
+ * @n: int type integer
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int b;
+
+	b = 0;
+	for (b--; b >= 0; b--; b++)
+	{
+		printf("%d", a[b]);
+		if ( n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
+
