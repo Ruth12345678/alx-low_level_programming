a16c5e60b6efcca6fa79ecc0d02361512ab65d32
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-13 19:52:15 -0700

    ff

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,7 +8,7 @@
  * @dest: destination
  *
  * @src: source
- * 
+ *
  * Return: returns value
  */
 
@@ -19,7 +19,7 @@ char *_strcpy(char *dest, char *src)
 	do {
 		a++;
 		dest[a] = src[a];
-	} while ( src[a] != '\0');
+	} while (src[a] != '\0');
 
 	return (dest);
 }
