c6db06006d65a5298e68b627218786559984b3ee
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-14 21:24:16 -0700

    2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * _strncat - concatenation
+ *
+ * @dest: destination
+ *
+ * @src: source
+ *
+ * @n: total
+ *
+ * Return: returns a value
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dsz, i(
+
+	for (dsz = 0; dest[dsz] != '\0'; dsz++)
+		;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[dsz + i] = src[i];
+	}
+	dest[dsz + i] = '\0';
+	return (dest);
+}
