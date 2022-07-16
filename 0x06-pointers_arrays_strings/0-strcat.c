b3b4525f3108990ec0673d4b73f981a2daf9a482
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-16 01:13:26 -0700

    gitt

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include<string.h>
 /**
  * _strcat - concatenation
  *
@@ -15,7 +15,8 @@ char *_strcat(char *dest, char *src)
 	int a = -1, i;
 
 	for (i = 0; dest[i] != '\0'; i++)
-	;
+	{
+	}
 	do {
 		a++;
 		dest[i] = src[a];
