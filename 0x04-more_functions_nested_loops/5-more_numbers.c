1a110e97ee7c2189e604647e626009722b3772d7
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-24 18:40:30 -0700

    more_numbers.2

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,19 +8,13 @@
 
 void more_numbers(void)
 {
-	int count;
-	char count2;
-	char num;
+	int count, num;
 
 	for (count = 0; count <= 10; count++)
 	{
-		for (num = '0'; num <= '9'; num++)
+		for (num = 0; num <= 14; num++)
 		{
-			_putchar(num);
-		}
-		for (count2 = '0'; count2 <= '4'; count2++)
-		{
-			printf("1%c", count2);
+			printf("%i", num);
 		}
 		putchar('\n');
 	}
