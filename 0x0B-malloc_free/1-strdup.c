d2cdf8f5ce60964bd14d45fe066c3df7e73ff8e7
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-26 17:32:50 -0700

    1-strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a copy of a string
+ *
+ * @str: string to be copied
+ *
+ * Return: returns a pointer to a string
+ */
+
+char *_strdup(char *str)
+{
+	char *stra;
+	int a, alen = strlen(str) + 1;
+
+	a = 0;
+	if (str != NULL)
+	{
+		stra = malloc(sizeof(stra) * 1);
+		for (a = 0; a < alen; a++)
+		{
+			stra[a] = str[a];
+		}
+	}
+	else
+	{
+		printf("Failed to allocate memory after %d calls", a);
+		return (NULL);
+	}
+	return (stra);
+}
