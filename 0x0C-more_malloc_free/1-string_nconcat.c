b52a4843cd489143427fe60e3f3fd20df4ba9788
Ruth Ogadina <ruthogadina@yahoo.com>
2022-07-29 17:45:47 -0700

    juiv

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,39 +20,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *news;
 	unsigned int a;
 	unsigned int b;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	news = malloc(sizeof(s1) + n);
-	if (news != NULL)
-	{
-		for (a = 0; a < strlen(s1); a++)
-		{
-			news[a] = s1[a];
-		}
-		if (n < strlen(s2))
-		{
-			for (b = 0; b < n; b++)
-			{
-				news[a] = s2[b];
-				a++;
-			}
-		}
-		else if (n >= strlen(s2))
-		{
-			for (b = 0; b < strlen(s2); b++)
-			{
-				news[a] = s2[b];
-				a++;
-			}
-		}
-		news[a] = '\0';
-	}
+	if (n < strlen(s2))
+		news = malloc(strlen(s1) + n * sizeof(char) + 1);
 	else
+		news = malloc(strlen(s1) + strlen(s2) + 1);
+	if (news == 0)
 	{
 		return (NULL);
 	}
+	for (a = 0; s1[a] != '\0'; a++)
+		news[a] = s1[a];
+	for (b = 0; s2[b] != '\0' && b < n; a++; b++)
+		news[a] = s2[b];
+	news[a] = '\0';
 	return (news);
 }
