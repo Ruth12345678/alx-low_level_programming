39f918492f1aae5ed4519f85112eb5f1f4c5087f
Ruth Ogadina <ruthogadina@yahoo.com>
2022-08-10 22:28:41 -0700

    var func

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include<stdio.h>
+#include<string.h>
+#include<stdarg.h>
+
+/**
+ * print_numbers - prints numbers
+ *
+ * @separator: separates stuff
+ *
+ * @n: number
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	if (separator != NULL)
+	{
+		va_list c;
+		unsigned int a;
+
+		va_start(c, n);
+
+		for (a = 0; a < n; a++)
+		{
+			if (a < n - 1)
+			{
+				printf("%d%s", va_arg(c, unsigned int), separator);
+			}
+			else
+				printf("%d", va_arg(c, unsigned int));
+		}
+		va_end(c);
+		putchar('\n');
+	}
+}
